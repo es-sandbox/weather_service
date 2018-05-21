3af4fc68adb8cf60dbb1a7baba379ebc1ae3b66d
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2018-05-21 17:36:31 +0300

    Added JS's chart rendering

--- public/draw.js
+++ public/draw.js
@@ -1,5 +1,29 @@
-TESTER = document.getElementById('tester');
-Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-    margin: { t: 0 } } );
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://localhost:8080/data.json', true);
+
+// xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+xhr.send(); // (1)
+
+var x = [], y = [];
+
+xhr.onreadystatechange = function() { // (3)
+    if (xhr.readyState != 4) return;
+
+    if (xhr.status != 200) {
+        console.log(xhr.status + ': ' + xhr.statusText);
+    } else {
+        console.log(xhr.responseText);
+
+        var weatherInfo = JSON.parse(xhr.responseText);
+
+        weatherInfo.forEach(function(item, i, arr) {
+            x.push(item.ID);
+            y.push(item.Temp);
+        });
+
+        TESTER = document.getElementById('tester');
+        Plotly.plot( TESTER, [ {x, y} ], { margin: { t: 0 } } );
+    }
+};
