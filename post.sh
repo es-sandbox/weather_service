e2a1c2d3b4bcf3cb20719a18d827298c06df9a53
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2018-05-24 20:39:27 +0300

    updated default location

--- post.sh
+++ post.sh
@@ -3,4 +3,4 @@
 PORT=${1:-9000}
 
 curl -XPOST localhost:$PORT/data -H "application/json" -d \
-    '{"TempOUT": 22, "Humidity": 12, "TempIN": 22.50,  "Pressure": 910.22, "WindSpeed": 12.50, "WindDirection": 12, "Rainfall": 10, "Battery": 72, "Thunder": 1, "Light": 52.2, "Charging": 1}'
+    '{"TempOUT": 22, "Humidity": 12, "TempIN": 22.50,  "Pressure": 910.22, "WindSpeed": 12.50, "WindDirection": 12, "Rainfall": 10, "Battery": 72, "Thunder": 1, "Light": 52.2, "Charging": 1, "Fire": true, "Snow": true, "SOS": true, "Latitude": 47.6414021, "Longitude": 26.2440167}'
